a22946517f8f4accc5f71ec1bee28f9a58acd40b
Riley Flynn <riley@rileyflynn.me>
2024-06-18 20:21:11 -0230

    Dark mode, basic list

--- shopkeeper/web/frontend/src/routes/root.tsx
+++ shopkeeper/web/frontend/src/routes/root.tsx
@@ -1,3 +1,18 @@
+import { useGetListings } from '../queries/api/shopkeeperComponents';
+
 export default function RootRoute() {
-    return <div>Hello world!</div>;
+    const { data: listings } = useGetListings({});
+
+    return (
+        <div className="p-2">
+            <ul>
+                {listings &&
+                    listings.map((listing) => (
+                        <li className="list-inside list-disc" key={listing.id}>
+                            {listing.title}
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
 }
