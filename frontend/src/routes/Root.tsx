9d682cfb095ae5d768f109c770fca4dbdce754c2
Riley Flynn <riley@rileyflynn.me>
2024-07-01 16:07:12 -0230

    Fix header alignment

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -5,7 +5,7 @@ export default function RootRoute() {
     return (
         <div>
             <header className="flex w-full flex-row justify-between p-2">
-                <h1 className="text-xl font-semibold">Shopkeeper</h1>
+                <h1 className="content-center text-xl font-semibold">Shopkeeper</h1>
                 <div>
                     <CreateListingDialog />
                 </div>
