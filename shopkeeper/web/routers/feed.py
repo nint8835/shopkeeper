e89bcfaa59f8edff55076093e28928d18cd2ab78
Riley Flynn <riley@rileyflynn.me>
2025-04-16 20:06:33 -0230

    Hide hidden listings; Incorporate time

--- shopkeeper/web/routers/feed.py
+++ shopkeeper/web/routers/feed.py
@@ -1,25 +1,49 @@
 from fastapi import APIRouter, Depends, Request
-from fastapi_rss import Item, RSSFeed, RSSResponse  # type: ignore
-from sqlalchemy import select
+from fastapi_rss import GUID, Item, RSSFeed, RSSResponse  # type: ignore
+from sqlalchemy import not_, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
+from shopkeeper.models.listing import Listing
 from shopkeeper.models.listing_event import ListingEvent
 from shopkeeper.web.dependencies.database import get_db
 
 feed_router = APIRouter(tags=["Feed"], include_in_schema=False)
 
 
+# TODO: Add auth
 @feed_router.get("/")
 async def get_feed(
     request: Request,
     db: AsyncSession = Depends(get_db),
 ) -> RSSResponse:
-    events = (await db.execute(select(ListingEvent))).scalars().all()
-    items = [Item(title=f"{event.from_value} -> {event.to_value}") for event in events]
+    events = (
+        (
+            await db.execute(
+                select(ListingEvent)
+                .options(joinedload(ListingEvent.listing))
+                .join(Listing)
+                .filter(not_(Listing.is_hidden))
+            )
+        )
+        .scalars()
+        .all()
+    )
+    items = [
+        Item(
+            guid=GUID(content=str(event.id)),
+            title=f"{event.from_value} -> {event.to_value}",
+            pub_date=event.time,
+        )
+        for event in events
+    ]
 
     return RSSResponse(
         RSSFeed(
-            title="Shopkeeper", description="", link=str(request.base_url), item=items
+            title="Shopkeeper",
+            description="Listing events for Shopkeeper",
+            link=str(request.base_url),
+            item=items,
         )
     )
 
