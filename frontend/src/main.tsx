f909d920842607a912e0e4464254117bd034f121
Riley Flynn <riley@rileyflynn.me>
2025-07-27 11:23:03 -0230

    Basic loading spinner, slight UI fixes, WIP wiring up of navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
+import { Spinner } from '@heroui/react';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { routeTree } from './routeTree.gen';
 
-const router = createRouter({ routeTree });
+function LoadingSpinner() {
+    return (
+        <div className="flex h-full flex-grow items-center justify-center">
+            <Spinner size="lg" />
+        </div>
+    );
+}
+
+const router = createRouter({ routeTree, defaultPendingComponent: LoadingSpinner });
 
 declare module '@tanstack/react-router' {
     interface Register {
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { fetchGetCurrentUser } from '@/queries/api/shopkeeperComponents';
 import { HeroUIProvider, ToastProvider } from '@heroui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Outlet, createRootRoute, redirect } from '@tanstack/react-router';
+import { NavigateOptions, Outlet, ToOptions, createRootRoute, redirect, useRouter } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
@@ -25,9 +25,21 @@ export const Route = createRootRoute({
     },
 });
 
+declare module '@react-types/shared' {
+    interface RouterConfig {
+        href: ToOptions['to'];
+        routerOptions: Omit<NavigateOptions, keyof ToOptions>;
+    }
+}
+
 function RootComponent() {
+    const router = useRouter();
+
     return (
-        <HeroUIProvider>
+        <HeroUIProvider
+            navigate={(to, options) => router.navigate({ to, ...options })}
+            useHref={(to) => router.buildLocation({ to }).href}
+        >
             <QueryClientProvider client={queryClient}>
                 <Outlet />
                 <ToastProvider />
